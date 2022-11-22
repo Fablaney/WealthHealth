4e5d425ea7e6a6051cf6a3cd1e58187b030d1a7e
Fablaney <fabrmoto@gmail.com>
2022-11-22 16:22:44 +0100

    commit

--- WealthHealth-New/src/components/DisplayTable.jsx
+++ WealthHealth-New/src/components/DisplayTable.jsx
@@ -1,102 +1,95 @@
-import { useTable, useSortBy } from "react-table"
-import { useMemo } from "react"
-import { useGlobalFilter } from 'react-table'
-import { useFilters } from 'react-table';
+import { useMemo } from 'react'
+import { useTable } from "react-table"
+import { useSortBy } from "react-table"
 
 function DisplayTable({colonnes, lignes})
 {
     console.log("j'entre dans react table")
     // console.log(colonnes)
     // console.log(lignes)
-
-    const FilterForm = ({ column }) => {
-        const { filterValue, setFilter } = column;
-        return (
-            <span>
-                <input
-                    value={filterValue || ''}
-                    onChange={(e) => setFilter(e.target.value)}
-                />
-            </span>
-        );
-    };
-    
-    colonnes.forEach(element => {
-        const filtre = { Filter: FilterForm}
-        Object.assign(element, filtre);
-    })
-
     const columns = useMemo(() => colonnes, [])
 
     const data = useMemo(() => lignes, [])
+
+    // const FilterForm = ({ column }) => {
+    //     const { filterValue, setFilter } = column;
+    //     return (
+    //         <span>
+    //             <input
+    //                 value={filterValue || ''}
+    //                 onChange={(e) => setFilter(e.target.value)}
+    //             />
+    //         </span>
+    //     );
+    // };
+    
+    // colonnes.forEach(element => {
+    //     const filtre = { Filter: FilterForm}
+    //     Object.assign(element, filtre);
+    // })
+
+    const tableInstance = useTable({ columns, data })
 
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
         rows,
-        state,
-        setGlobalFilter,
         prepareRow,
-    } = useTable({ columns, data }, useFilters, useGlobalFilter, useSortBy);
-
-    const { globalFilter } = state;
-
+    } = useTable({columns,data,},
+        useSortBy
+    )
  
     return (
         <div className="container">
 
-            <div className="search-container">
-                Search
-                &nbsp;
-                <input
-                    type="text"
-                    value={globalFilter || ''}
-                    onChange={(e) => setGlobalFilter(e.target.value)}
-                />
-            </div>
-
             <table {...getTableProps()} className="table">
-
                 <thead>
-                    {   
-                        headerGroups.map((headerGroup) => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map((column) => (
-                                    console.log(column)
-                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                        {column.render('Header')}
-                                        <span>
-                                            {column.isSorted
-                                                ? Column.isSortedDesc
-                                                    ?' 🔽'
-                                                    : '🔼'
-                                                :""}
-                                        </span>
-                                    </th>
-                                ))}
-                            </tr>
-                        ))
-                    }
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        // Add the sorting props to control sorting. For this example
+                        // we can add them into the header props
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                        {column.render('Header')}
+                        {/* Add a sort direction indicator */}
+                        <span>
+                            {column.isSorted
+                            ? column.isSortedDesc
+                                ? ' 🔽'
+                                : ' 🔼'
+                            : ''}
+                        </span>
+                        </th>
+                    ))}
+                    </tr>
+                ))}
                 </thead>
-
+                {/* Apply the table body props */}
                 <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row);
+                {// Loop over the table rows
+                rows.map(row => {
+                    // Prepare the row for display
+                    prepareRow(row)
+                    return (
+                    // Apply the row props
+                    <tr {...row.getRowProps()}>
+                        {// Loop over the rows cells
+                        row.cells.map(cell => {
+                        // Apply the cell props
                         return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => {
-                                    return (
-                                        <td {...cell.getCellProps()}>
-                                            {cell.render('Cell')}
-                                        </td>
-                                    );
-                                })}
-                            </tr>
-                        );
-                    })}
+                            <td {...cell.getCellProps()}>
+                            {// Render the cell contents
+                            cell.render('Cell')}
+                            </td>
+                        )
+                        })}
+                    </tr>
+                    )
+                })}
                 </tbody>
             </table>
+             
         </div>
     )
 }
