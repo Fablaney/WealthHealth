8cfb6e0c38a7b2d087dcaf357cb44f51e1444c17
Fablaney <fabrmoto@gmail.com>
2022-12-22 12:39:05 +0100

    commit

--- WealthHealth-New/src/pages/Home.jsx
+++ WealthHealth-New/src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home()
 
     const onSubmit = data => {
         dispatch(employeeActions.addEmployee(data))
-        console.log(data)
+        // console.log(data)
         document.querySelector(".modale").classList.remove("d-none")
     }
 
